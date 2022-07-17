17ba6604a3c26eb8baeeebf210ae03a27799c515
dmi3y-v <dmi3y.vagin@yandex.ru>
2022-07-17 16:19:14 +0300

    wip

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
-  return <div></div>;
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    setTimeout(() => {
+      setData({});
+    }, 100);
+  }, []);
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <button>click me</button>
+      <input type="text" placeholder="input value..."></input>
+      {data && <div>data</div>}
+    </div>
+  );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("TEST APP", () => {
+  test("renders main App", () => {
+    render(<App />);
+    const helloWorldElement = screen.getByText(/hello world/i);
+    const btn = screen.getByRole("button");
+    const input = screen.getByPlaceholderText(/input value/i);
+    expect(helloWorldElement).toBeInTheDocument();
+    expect(btn).toBeInTheDocument();
+    expect(input).toMatchSnapshot();
+  });
+  test("renders main App", async () => {
+    render(<App />);
+    // const helloWorldElement = screen.queryByText(/hello2/i);
+    // expect(helloWorldElement).toBeNull();
+    screen.debug();
+    const helloWorldElement = await screen.findByText(/data/i);
+    expect(helloWorldElement).toBeInTheDocument();
+    screen.debug();
+  });
 });
